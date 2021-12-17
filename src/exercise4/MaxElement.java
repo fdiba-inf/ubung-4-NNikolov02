433fa10a8bc8253cb119a8f257ddc3655326e668
NNikolov02 <>
2021-12-17 21:38:07 +0000

    Ubung4

--- src/exercise4/MaxElement.java
+++ src/exercise4/MaxElement.java
@@ -1,29 +1,27 @@
 package exercise4;
 
+import java.lang.Math;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class  MaxElement  {
+public class MaxElement {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double number = - 9999;
         
-        int  size = input.nextInt();
-        double [] array = new double[size];
-        
-        for(int index = 0;index < size ;index++){
-        array[index] = input.nextInt();
-        
-        if(array[index] >  number){
-          number = array[index];
-        } 
+        double number = -9999;
+        int size = input.nextInt();
+        double[] numbers = new double[size];
+
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = input.nextInt();
+            if (numbers[index] > number){
+              number = numbers[index];
+            }
         }
-        System.out.println("Max number:" + number);
+        System.out.println("Max number: " + number);
     }
-}
-
-
+}  
 
         
   
